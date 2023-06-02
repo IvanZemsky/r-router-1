13c276b2fd6fbcd0dacb3daec61172a3fd394d6a
IvanZemsky <i.ivanzemsky2005@gmail.com>
2023-06-02 21:42:06 +0300

    leson 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route, Link} from 'react-router-dom'
+import Aboutpage from './components/Aboutpage.js'
+import Blogpage from './components/Blogpage.js'
+import Homepage from './components/Homepage.js'
+import Notfoundpage from './components/Notfoundpage.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Blog</Link>
+        <Link to="/about">About</Link>
       </header>
-    </div>
+      <div>
+        <h1>Get started with React Router V6</h1>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/posts" element={<Blogpage/>}/>
+        <Route path="/about" element={<Aboutpage/>}/>
+        <Route path="*" element={<Notfoundpage/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Notfoundpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>Not found</div>
+  )
+}
+
+export default Homepage
