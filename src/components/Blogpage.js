94ea1a450e0ecc9fe8bf960f412d3dd181ba24dc
IvanZemsky <i.ivanzemsky2005@gmail.com>
2023-06-16 21:12:30 +0300

    lesson 6

--- src/components/Blogpage.js
+++ src/components/Blogpage.js
@@ -1,10 +1,20 @@
+import { getByPlaceholderText } from '@testing-library/react';
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import { BlockFilter } from './BlockFilter';
 
 function Blogpage() {
 
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get('post') || '';
+  // URL.ru/post?post=abc&data=
+
+  const latest = searchParams.has('latest');
+
+  const startsFrom = latest ? 80 : 1;
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -14,10 +24,13 @@ function Blogpage() {
 
   return (
     <div>
-      Blogpage
+      <h1>Blogpage</h1>
+      <BlockFilter postQuery={postQuery} latest={latest} setSearchParams={setSearchParams}/>
       <Link to='/posts/new'>Add new post</Link>
       {
-        posts.map(post => (
+        posts.filter(
+          post => post.title.includes(postQuery) && post.id >= startsFrom
+        ).map(post => (
           <Link key={post.id} to={`/posts/${post.id}`}>
             <li>{post.title}</li>
           </Link>
